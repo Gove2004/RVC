f9da087aa70f5545115433af4d31effb18930b75
Gove <3425519400@qq.com>
2026-09-08 16:36:42 +0800

    修复训练加载预训练底模 KeyError: weight 键名改为 model

--- rvc/train/trainer.py
+++ rvc/train/trainer.py
@@ -93,11 +93,11 @@ class Trainer:
         else:
             if self.cfg.pretrain_g:
                 state = torch.load(self.cfg.pretrain_g, map_location="cpu", weights_only=False)
-                self.synthesizer.load_state_dict(state["weight"], strict=False)
+                self.synthesizer.load_state_dict(state["model"], strict=False)
                 self.log("加载预训练 G")
             if self.cfg.pretrain_d:
                 state = torch.load(self.cfg.pretrain_d, map_location="cpu", weights_only=False)
-                self.net_d.load_state_dict(state["weight"], strict=False)
+                self.net_d.load_state_dict(state["model"], strict=False)
                 self.log("加载预训练 D")
 
         self.scheduler_g = torch.optim.lr_scheduler.ExponentialLR(self.optim_g, gamma=self.train_cfg["lr_decay"], last_epoch=self.start_epoch - 2)
