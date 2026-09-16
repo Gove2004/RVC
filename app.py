0464f52f763431c938c39e517c119748b0d6e0ca
Gove <3425519400@qq.com>
2026-09-16 09:51:44 +0800

    refactor: step8 reorganize gui/train
    - gui/train/view/main_window.py -> gui/train/view/window.py
    - gui/configs/train_state.py -> gui/train/state/train_state.py
    - Update gui.configs.__init__ re-export path
    - Update app.py import
    - No behavior change

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def main():
         from gui.infer.view.window import MainWindow
         win = MainWindow()
     else:
-        from gui.train.view.main_window import TrainWindow
+        from gui.train.view.window import TrainWindow
         win = TrainWindow()
 
     win.show()
